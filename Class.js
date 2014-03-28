d872762c437b1a2b5881cbfc76945ff43e66a477
Ming-Zhe <law.gravitys@gmail.com>
2014-03-28 10:15:56 +0800

    instance-methods

--- Class.js
+++ Class.js
@@ -1,12 +1,51 @@
-var Class = function(argv) {
-  if (typeof argv['initialize'] === 'undefined')
+var Class = function(param) {
+  if (typeof param['initialize'] === 'undefined')
     return function(){};
   
-  var constructor = argv['initialize'];
+  var constructor = param['initialize'];
+  constructor.prototype.getA = function() {
+  	return this.a;
+  };
+  constructor.prototype.getB = function() {
+  	return this.b;
+  };
+  // console.log(constructor);
   return constructor;
 }
 
-var Foo = Class({initialize: constructor});
-Foo();
+// var constructor = function(a,b) {
+//     this.a = a;
+//     this.b = b;
+//   };
+// var Foo = Class({initialize: constructor});
+// Foo();
+
+var Foo = Class({
+    initialize: function(a,b) {
+      this.a = a;
+      this.b = b;
+    },
+
+    getA: function() {
+      return this.a;
+    },
+
+    getB: function() {
+      return this.b;
+    }
+  });
+
+var foo = new Foo(1,2);
+var num = foo.getA;
+// console.log(foo.getA);
 
 module.exports = Class;
+
+
+
+
+
+
+
+
+
