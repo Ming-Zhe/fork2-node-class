4f8ffafd7b83b6d357fbb377b724556847cde2ef
Ming-Zhe <law.gravitys@gmail.com>
2014-03-28 11:52:15 +0800

    modified constructor

--- Class.js
+++ Class.js
@@ -2,15 +2,15 @@ var Class = function(param) {
   if (typeof param['initialize'] === 'undefined')
     return function(){};
   
-  var constructor = param['initialize'];
-  constructor.prototype.getA = function() {
+  var construct = param['initialize'];
+  this.constructor.prototype.getA = function() {
   	return this.a;
   };
-  constructor.prototype.getB = function() {
+  this.constructor.prototype.getB = function() {
   	return this.b;
   };
-  // console.log(constructor);
-  return constructor;
+  // console.log(construct);
+  return construct;
 }
 
 // var constructor = function(a,b) {
@@ -20,23 +20,23 @@ var Class = function(param) {
 // var Foo = Class({initialize: constructor});
 // Foo();
 
-var Foo = Class({
-    initialize: function(a,b) {
-      this.a = a;
-      this.b = b;
-    },
+// var Foo = Class({
+//     initialize: function(a,b) {
+//       this.a = a;
+//       this.b = b;
+//     },
 
-    getA: function() {
-      return this.a;
-    },
+//     getA: function() {
+//       return this.a;
+//     },
 
-    getB: function() {
-      return this.b;
-    }
-  });
+//     getB: function() {
+//       return this.b;
+//     }
+//   });
 
-var foo = new Foo(1,2);
-var num = foo.getA;
+// var foo = new Foo(1,2);
+// var num = foo.getA;
 // console.log(foo.getA);
 
 module.exports = Class;
