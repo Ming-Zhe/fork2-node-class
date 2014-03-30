f8e6ff5d7c414554afe1ab6376b43d5adaa88127
Ming-Zhe <law.gravitys@gmail.com>
2014-03-30 21:34:19 +0800

    do not understand

--- Class.js
+++ Class.js
@@ -1,51 +1,48 @@
-var Class = function(param) {
-  if (typeof param['initialize'] === 'undefined')
-    return function(){};
-  
-  var construct = param['initialize'];
-  this.constructor.prototype.getA = function() {
-  	return this.a;
-  };
-  this.constructor.prototype.getB = function() {
-  	return this.b;
-  };
-  // console.log(construct);
-  return construct;
+var Class = function (param, parent){
+
+	var construct;
+	if (typeof param['initialize'] === 'undefined')
+		construct = function(){};
+	else
+		construct = param['initialize'];
+
+	if (parent){
+		for (var key in parent.prototype){
+			construct.prototype[key] = parent.prototype[key];
+		}
+		construct.__super__ = parent;
+	}else{
+		construct.__super__ = Object;
+	}
+	for(var key in param){
+		if(key != 'initialize'){
+			construct.prototype[key] = param[key];
+		}
+	}
+
+	var current_class = construct;
+  	construct.prototype.super = function(){
+  		current_class = current_class.__super__;
+	    res = current_class.prototype[arguments[0]].apply(this, [].slice.call(arguments, 1));
+	    current_class = construct;
+	    return res;
+  	}
+
+	// construct.prototype.super = function(){
+	// 	if (!arguments){
+	// 		return construct.__super__.prototype['self'].apply(this,[].slice.call(arguments, 1));
+	// 	}
+	// 	if (arguments.length == 1){
+	// 		return construct.__super__.prototype[arguments[0]];
+	// 	}
+	// }
+	
+	construct.prototype.constructor = construct;
+	return construct;
 }
 
-// var constructor = function(a,b) {
-//     this.a = a;
-//     this.b = b;
-//   };
-// var Foo = Class({initialize: constructor});
-// Foo();
-
-// var Foo = Class({
-//     initialize: function(a,b) {
-//       this.a = a;
-//       this.b = b;
-//     },
-
-//     getA: function() {
-//       return this.a;
-//     },
-
-//     getB: function() {
-//       return this.b;
-//     }
-//   });
-
-// var foo = new Foo(1,2);
-// var num = foo.getA;
-// console.log(foo.getA);
 
 module.exports = Class;
-
-
-
-
-
-
 
 
 
